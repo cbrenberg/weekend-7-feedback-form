7c270c377f26e244974e7e50db7224a8361cdca1
Christopher Brenberg <cbrenberg@gmail.com>
2018-10-12 15:36:09 -0500

    set up redux store and test reducers in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//reducer functions below
+const feedbackItem = (state={}, action) => {
+  return state;
+}
+
+
+const allReducers = combineReducers({
+  feedbackItem,
+})
+
+const store = createStore(
+  allReducers,
+  applyMiddleware(logger),
+)
+
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
